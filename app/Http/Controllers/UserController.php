600191c67c8d3d316d08b33bc01f66d51bfdeec0
Manuel Tsvetanski <manuel.tsvetanski@gmail.com>
2024-07-19 02:30:56 +0300

    TSK-001 | Update UserController to match provided JSON structure

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,22 +9,85 @@ class UserController extends Controller
 {
     private $users = [
         [
-            'id' => 1,
-            'username' => 'Alice',
+            'id' => 5,
+            'username' => 'Test 5',
             'logins' => [
-                ['datetime' => '2024-07-17T15:24:00'],
-                ['datetime' => '2024-07-16T14:23:00'],
-                ['datetime' => '2024-07-15T13:22:00'],
+                ['datetime' => '2023-12-21 18:29'],
+                // Add more login datetimes if needed
             ],
         ],
         [
-            'id' => 2,
-            'username' => 'Bob',
+            'id' => 4,
+            'username' => 'Test 4',
             'logins' => [
-                ['datetime' => '2024-07-17T16:24:00'],
-                ['datetime' => '2024-07-15T12:22:00'],
+                ['datetime' => '2023-12-21 18:27'],
+                // Add more login datetimes if needed
             ],
-        ]
+        ],
+        [
+            'id' => 42,
+            'username' => 'Test 42',
+            'logins' => [
+                ['datetime' => '2023-12-21 18:24'],
+                // Add more login datetimes if needed
+            ],
+        ],
+        [
+            'id' => 22,
+            'username' => 'Test 22',
+            'logins' => [
+                ['datetime' => '2023-12-21 18:20'],
+                // Add more login datetimes if needed
+            ],
+        ],
+        [
+            'id' => 829,
+            'username' => 'Test 829',
+            'logins' => [
+                ['datetime' => '2023-12-21 18:15'],
+                // Add more login datetimes if needed
+            ],
+        ],
+        [
+            'id' => 15,
+            'username' => 'Test 15',
+            'logins' => [
+                ['datetime' => '2023-12-21 18:09'],
+                // Add more login datetimes if needed
+            ],
+        ],
+        [
+            'id' => 14,
+            'username' => 'Test 14',
+            'logins' => [
+                ['datetime' => '2023-12-21 18:02'],
+                // Add more login datetimes if needed
+            ],
+        ],
+        [
+            'id' => 142,
+            'username' => 'Test 142',
+            'logins' => [
+                ['datetime' => '2023-12-21 17:54'],
+                // Add more login datetimes if needed
+            ],
+        ],
+        [
+            'id' => 122,
+            'username' => 'Test 122',
+            'logins' => [
+                ['datetime' => '2023-12-21 17:45'],
+                // Add more login datetimes if needed
+            ],
+        ],
+        [
+            'id' => 1829,
+            'username' => 'Test 1829',
+            'logins' => [
+                ['datetime' => '2023-12-21 17:35'],
+                // Add more login datetimes if needed
+            ],
+        ],
     ];
 
     private function getLastLogin($user)
